43299e4882ef9b953e9688c8fe1e9c6ed6a5e230
Daniela2319 <frankvt04@gmail.com>
2022-11-06 20:25:11 -0300

    collection Map

--- src/main/java/collectionMap/model/LivroMain.java
+++ src/main/java/collectionMap/model/LivroMain.java
@@ -1,9 +1,6 @@
 package collectionMap.model;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class LivroMain {
 
@@ -24,14 +21,21 @@ public class LivroMain {
         Map<String,Livro> meusLivros1 = new LinkedHashMap<>(){{
             put("Frei Romano Zago", new Livro("Babosa não é remédio... mas cura!",125));
             put("Caíque Cardoso", new Livro("OO na Prática",175));
-            put("Mayra, Gustavo", new Livro("O Reizinho Autista",108));
+            put("Mayra, Gustavo", new Livro(" Reizinho Autista",108));
 
         }};
         for (Map.Entry<String,Livro> livros : meusLivros.entrySet())
             System.out.println(livros.getKey() + " - " + livros.getValue().getNome());
 
+        System.out.println("--\tOrdem alfabética autores\t--");
+        Map<String,Livro>meusLivrosI = new TreeMap<>(meusLivros1);
+        for (Map.Entry<String,Livro> livros : meusLivrosI.entrySet())
+            System.out.println(livros.getKey() + " - " + livros.getValue().getNome());
 
-
-
+        System.out.println("\tOrdem alfabética nomes dos livros\t");
+        Set<Map.Entry<String, Livro>> meusLivrosII = new TreeSet<>( new ComparatorNome());
+        meusLivrosII.addAll(meusLivros.entrySet());
+        for (Map.Entry<String,Livro> livros : meusLivrosII)
+            System.out.println(livros.getKey() + " - " + livros.getValue().getNome());
     }
 }
